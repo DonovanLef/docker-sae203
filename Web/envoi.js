aedbe9e3906cb37b91ef3a127228753080f3514c
Lefevre Donovan <lfv.donovan@gmail.com>
2023-04-12 10:52:04 +0200

    init

--- Web/envoi.js
+++
@@ -1,9 +0,0 @@
-function envoiNouveauMessage( content ) {
-	websocket.send( `{ "type": "newMsg", "content": "${content}"}` );
-	inputSendMsg.value = "";
-}
-
-function envoiVote( idMusique ) {
-	websocket.send( `{ "type": "newVote", "content": ${idMusique}}` );
-}
-
