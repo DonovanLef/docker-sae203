68303413a3bc9fae33ec2eec3a2cd8280641c2a4
Lefevre Donovan <lfv.donovan@gmail.com>
2023-05-03 12:49:22 +0200

    Rectification indentation

--- back/java/handle/HandleReceive.java
+++ back/java/handle/HandleReceive.java
@@ -57,7 +57,7 @@ public class HandleReceive
 		User user = new User( this.jsonNode.get("content").textValue() );
 		user.setSession( session );
 		SocketUtil.addUser( user );
-	};
+	}
 
 	/* Methode permettant d'envoyer le message d'un utilisateur, elle recupère le nom
 	   de l'utilisateur et l'heure a laquelle le message a ete envoye et verifie le message 
@@ -70,7 +70,7 @@ public class HandleReceive
 		if ( !Util.verifMessage(msg) ) return;
 		Message message = new Message( msg, name, heure );
 		HandleSend.sendMsg( message );
-	};
+	}
 
 	/* Methode permettant a un utilisateur de voter pour une musique, elle verifie que
 	   l'ancien vote de l'utilisateur n'est pas le même que le nouveau. Cette methode appelle
@@ -88,7 +88,7 @@ public class HandleReceive
 		user.setVote( newMusicVote );
 
 		HandleSend.sendAllMusic();
-	};
+	}
 
 	public static void importMusic( String title, String author, String music)
 	{
